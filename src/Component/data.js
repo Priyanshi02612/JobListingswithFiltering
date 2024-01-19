7e63f00be9d99a853ea393eb7c6e89fe1198e3a6
Krina Beladiya <krinabeladiya@gmail.com>
2024-01-19 14:55:20 +0530

    first commit

---
+++ src/Component/data.js
@@ -0,0 +1,82 @@
+export const Data = [
+  {
+    "id" : "1",
+    "name": "Photosnap",
+    "image": "photosnap.svg",
+    "role": "Senior Frontend Developer",
+    "info": "1d ago - Full Time - USA only",
+    "skill": ["Frontend", "Senior", "HTML", "CSS", "JavaScript"]
+  },
+  {
+    "id" : "2",
+    "name": "Manage",
+    "image": "manage.svg",
+    "role": "Fullstack Developer",
+    "info": "1d ago - Part Time - Remote",
+    "skill": ["Fullstack", "Midweight", "Python", "React"]
+  },
+  {
+    "id" : "3",
+    "name": "Account",
+    "image": "account.svg",
+    "role": "Junior Frontend Developer",
+    "info": "2d ago - Part Time - USA only",
+    "skill": ["Frontend", "Junior", "Sass", "React", "JavaScript"]
+  },
+  {
+    "id" : "4",
+    "name": "MyHome",
+    "image": "myhome.svg",
+    "role": "Junior Frontend Developer",
+    "info": "5d ago - Part Time - USA only",
+    "skill": ["Frontend", "Junior", "CSS", "JavaScript"]
+  },
+  {
+    "id" : "5",
+    "name": "Loop Studios",
+    "image": "loop-studios.svg",
+    "role": "Software Engineer",
+    "info": "1w ago - Full Time - Worldwide",
+    "skill": ["Fullstack", "Midweight", "JavaScript", "Sass", "Ruby"]
+  },
+  {
+    "id" : "6",
+    "name": "FaceIt",
+    "image": "faceit.svg",
+    "role": "Junior Backend Developer",
+    "info": "2w ago - Full Time - UK only",
+    "skill": ["Backend", "Junior", "Ruby", "RoR"]
+  },
+  {
+    "id" : "7",
+    "name": "Shortly",
+    "image": "shortly.svg",
+    "role": "Junior Developer",
+    "info": "2w ago - Full Time - Worldwide",
+    "skill": ["Frontend", "Junior", "HTML", "Sass", "JavaScript"]
+  },
+  {
+    "id" : "8",
+    "name": "Insure",
+    "image": "insure.svg",
+    "role": "Junior Frontend Developer",
+    "info": "2w ago - Full Time - USA only",
+    "skill": ["Frontend", "Junior", "Vue", "Sass", "JavaScript"]
+  },
+  {
+    "id" : "9",
+    "name": "Eyecam Co.",
+    "image": "eyecam-co.svg",
+    "role": "Full Stack Engineer",
+    "info": "3w ago - Full Time - Worldwide",
+    "skill": ["Fullstack", "Midweight", "JavaScript", "Django", "Python"]
+  },
+  {
+    "id" : "10",
+    "name": "The Air Filter Company",
+    "image": "the-air-filter-company.svg",
+    "role": "Front-end Dev",
+    "info": "1mo ago - Part Time - Worldwide",
+    "skill": ["Frontend", "Junior", "React", "Sass", "JavaScript"]
+  }
+]
